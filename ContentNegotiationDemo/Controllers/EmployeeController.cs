a98e5a3ea199300cbc667681d562dde754b40247
Daniel Ikechukwu <ikechukwudaniel080@gmail.com>
2025-05-08 13:06:18 +0100

    Add project files.

---
+++ ContentNegotiationDemo/Controllers/EmployeeController.cs
@@ -0,0 +1,24 @@
+﻿using ContentNegotiationDemo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContentNegotiationDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        //Get: api/employee
+        [HttpGet]
+        public ActionResult<List<Employee>> GetEmployees()
+        {
+            var listEmployee = new List<Employee>
+            {
+                new Employee(){ Id = 1001, Name = "Anurag", Age = 28, Gender = "Male", Department = "IT" },
+                new Employee(){ Id = 1002, Name = "Pranaya", Age = 28, Gender = "Male", Department = "IT" },
+            };
+
+            return Ok(listEmployee);
+        }
+    }
+}
---
+++ ContentNegotiationDemo/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿namespace ContentNegotiationDemo.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Gender { get; set; }
+
+        public int Age { get; set; }
+
+        public string Department { get; set; }
+    }
+}
