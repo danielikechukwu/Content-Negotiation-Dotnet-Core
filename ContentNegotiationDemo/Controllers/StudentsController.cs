ec57255f61653ef7aab59387e5f134db176a2aa4
Daniel Ikechukwu <ikechukwudaniel080@gmail.com>
2025-05-09 08:17:47 +0100

    feat(exclude): Implement the exclude of properties from model binding.

---
+++ ContentNegotiationDemo/Controllers/StudentsController.cs
@@ -0,0 +1,43 @@
+﻿using ContentNegotiationDemo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContentNegotiationDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        public static readonly List<Student> listStudents = new List<Student>
+        {
+            new Student(){ Id = 1, Name = "Anurag", Age = 28, Salary = 1000, Gender = "Male", Department = "IT" },
+            new Student(){ Id = 2, Name = "Pranaya", Age = 28, Salary = 2000, Gender = "Male", Department = "IT" },
+        };
+
+        // Returns the list of all employees.
+        // GET api/Employee
+        [HttpGet]
+        public ActionResult<List<Student>> GetStudents()
+        {
+            return Ok(listStudents);
+        }
+
+        [HttpPost]
+        public ActionResult<Student> AddStudent(Student student)
+        {
+
+            if (student != null)
+            {
+                //Manually setting the sensative properties to avoid clients manipulations.
+                student.Id = listStudents.Count + 1;
+                student.Salary = 3000;
+                listStudents.Add(student);
+                return Ok(student);
+            }
+
+            return BadRequest();
+        }
+
+
+    }
+}
---
+++ ContentNegotiationDemo/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿namespace ContentNegotiationDemo.Models
+{
+    public class Student
+    {
+        public int Id { get; set; } //Sensitive property
+
+        public string Name { get; set; }
+
+        public string Gender { get; set; }
+
+        public int Age { get; set; }
+
+        public int Salary { get; set; } //Sensitive property
+
+        public string Department { get; set; }
+
+    }
+}
